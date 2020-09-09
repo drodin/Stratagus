81a82b36905133546f3a50ff0ed6359416c80ef3
drodin <rodin.dmitry@gmail.com>
2020-09-09 11:47:35 +0700

    bump version number to v3.0.0-dev1

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,4 +1,10 @@
 hunter_config(
     Lua
-    VERSION 5.1.5-p3)
+    VERSION 5.1.5-p3
+)
 
+hunter_config(
+    SDL_mixer
+    VERSION 2.0.4-p0
+    CMAKE_ARGS "MUSIC_CMD=OFF" "MUSIC_FLAC=OFF" "MUSIC_OPUS=OFF"
+)
